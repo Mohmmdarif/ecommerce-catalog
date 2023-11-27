f2988139138c23e90430a0dd5817f95122c45169
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2023-11-27 22:25:34 +0700

    added loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="center">
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+    <div class="wave"></div>
+  </div>
+</template>
+
+<script>
+import "../assets/style/loader.css";
+
+export default {
+  name: "Loader",
+};
+</script>
